ba60cf00a71c1fcf054727a150674cc87fabe263
markeno_nagode@hotmail.com <markeno_nagode@hotmail.com>
2024-07-26 00:36:50 +0800

    almost done

--- app.py
+++ app.py
@@ -3,12 +3,17 @@ import pandas as pd
 import plotly.express as px
 import requests
 import re
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
-sample_file = 'data_example.xlsx'
+st.set_page_config(layout="wide", page_title="Dashboard", page_icon="📈")
 default_url = "https://script.googleusercontent.com/macros/echo?user_content_key=CS1xGtawxafbsz_VXLceDZ8YhZJ3WLRGo3cTurMdyLFpRky5qzeXoqZBrYuJ0ElreSkNapo1i9TRXgBoQpou4xhHPHjfNUj4m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnNGpsruz596piGbFJMaeWeZjFnhN0a9d4HwmOCyefi3ex5PPcpNsPlxGCUpGq7rwIA3gVkC6Pkbl8Md7CuyY-jH4-8QW02051A&lib=McNoStEX3P6w1EymfWUjOXqNKe58B53cM"
 template_list = [ "Last 3 Months", "Last 6 Months", "Last 9 Months", "Last 12 Months","Overall"]
-st.set_page_config(layout="wide", page_title="Dashboard", page_icon="📈")
+present = "出席"
+absent = "缺席"
+leave = "請假"
 
+@st.cache_data
 def getDataJson(url):
     resp = requests.get(url)
     resp_json = resp.json()
@@ -27,7 +32,7 @@ def transformData(df):
     cols = [cols[-1]] + cols[:-1]
     output_df = df[cols].copy()
     return output_df
-
+@st.cache_data
 def getDf_from_file(filePath):
     df = pd.read_excel(filePath)
     return transformData(df)
@@ -36,76 +41,70 @@ def getDf_from_google_sheet_json(resp_json):
     df = pd.DataFrame(resp_json['data'][1:], columns=resp_json['data'][0])
     return transformData(df)
 
+def filter_last_x_months(df, x):
+    today = datetime.today()
+    x_months_ago = today - relativedelta(months=x)
+    temp_df = df.copy()
+    temp_df['date'] = pd.to_datetime(temp_df['date'])
+    return df[temp_df['date'] >= x_months_ago]
+
+def getDf_present_rate(df, prenset, absent, leave):
+    result = pd.DataFrame()
+    for column in df.columns[2:]:
+        present_count = df[column].value_counts().get(prenset, 0)
+        absent_count = df[column].value_counts().get(absent, 0)
+        leave_count = df[column].value_counts().get(leave, 0)
+        total_count = present_count + absent_count + leave_count
+        present_rate = (present_count / total_count * 100) if total_count > 0 else 0
+        result.loc[column, 'Present Count'] = present_count
+        result.loc[column, 'Absent Count'] = absent_count
+        result.loc[column, 'Leave Count'] = leave_count
+        result.loc[column, 'Present Rate (%)'] = round(present_rate,2)
+    result['Present Count'] = result['Present Count'].astype(int)
+    result['Absent Count'] = result['Absent Count'].astype(int)
+    result['Leave Count'] = result['Leave Count'].astype(int)
+    return result
+
 def display_dashboard(dataFile: str = None):
     if dataFile:
         df = getDf_from_file(dataFile)
         df.fillna("", inplace=True)
     else:
-        print("it is none")
         df = getDf_from_google_sheet_json(getDataJson(default_url))
         
-
-    st.table(df)
-    # df_description_list = []
-    # for df in df_list:
-    #     grouped = df.groupby('Product')
-    #     description_per_product = grouped['Quantity'].describe()
-    #     df_description_list.append(description_per_product)
-
-    # df_grouped_list = []
-    # for df in df_list:
-    #     df_grouped = df.groupby("Product").sum()
-    #     df_grouped_list.append(df_grouped)
-    # concatenated_df = pd.concat(df_grouped_list, ignore_index=False, axis=1)
-    # concatenated_df.columns = [str(i+1) for i in range(12)]
-
-    # concatenated_df_total = concatenated_df.copy()
-    # sum_dict = {}
-    # for i in range(len(df_grouped_list)):
-    #     sum_dict[str(i+1)] = df_grouped_list[i]['Quantity'].sum()
-    # concatenated_df_total.loc['Total'] = sum_dict
-    # concatenated_df_total['Total'] = concatenated_df_total.sum(axis=1)
-
-    # concatenated_df = concatenated_df.reset_index()
-    # df_melted = concatenated_df.melt(id_vars='Product', var_name='Month', value_name='Sales')
-
-    # with st.container():
-    #     col1, col2 = st.columns(2)
-    #     with col1:      
-    #         # Create a line chart using Plotly Express
-    #         fig = px.line(df_melted, x='Month', y='Sales', color='Product')
-
-    #     # Set the x and y-axis labels
-    #         fig.update_layout(xaxis_title='Month', yaxis_title='Sales')
-
-    #     # Display the chart using Streamlit
-    #         st.plotly_chart(fig)
-    #     with col2:
-    #         fig2 = px.bar(df_melted, x="Month", y="Sales", color="Product")
-    #         fig2.update_layout(xaxis_title='Month', yaxis_title='Sales')
-    #         st.plotly_chart(fig2)
+    present_rate_df = getDf_present_rate(df, present, absent, leave)
+    filtered_3_df = filter_last_x_months(df,3)
+    present_rate_3_df = getDf_present_rate(filtered_3_df, present, absent, leave)
+    filtered_6_df = filter_last_x_months(df,6)
+    present_rate_6_df = getDf_present_rate(filtered_6_df, present, absent, leave)
+    filtered_9_df = filter_last_x_months(df,9)
+    present_rate_9_df = getDf_present_rate(filtered_9_df, present, absent, leave)
+    filtered_12_df = filter_last_x_months(df,12)
+    present_rate_12_df = getDf_present_rate(filtered_12_df, present, absent, leave)
+    df_description_list = [present_rate_3_df, present_rate_6_df, present_rate_9_df, present_rate_12_df, present_rate_df]
 
 # Display the DataFrame as a table with centering style
-    st.header('Present Rate Stats', divider='rainbow')
-    # for index, tab in enumerate(st.tabs(template_list)):
-    #     with tab:
-    #         st.header(template_list[index])
-    #         with st.container():
-    #             st.dataframe(df_description_list[index], use_container_width=True)
+    st.header('Present Rate', divider='rainbow')
+    for index, tab in enumerate(st.tabs(template_list)):
+        with tab:
+            st.header(template_list[index])
+            with st.container():
+                st.dataframe(df_description_list[index], use_container_width=True)
 
     st.header('Cumulative Sum', divider='rainbow')
-    # with st.container():
-    #     #st.dataframe(concatenated_df_total, use_container_width=True)
-    #     fig2 = px.bar(df_melted, x="Month", y="Sales", color="Product")
-    #     fig2.update_layout(xaxis_title='Month', yaxis_title='Sales')
-    #     st.plotly_chart(fig2)
+    with st.container():
+        plot_df = present_rate_df.copy()
+        plot_df.reset_index(inplace=True)
+        plot_df.columns = ['Name', 'Present Count', 'Absent Count', 'Leave Count', 'Present Rate (%)']
+        fig = px.histogram(plot_df, x = "Name", y = ["Present Count", "Absent Count", "Leave Count"])
+        st.plotly_chart(fig)
 
-        
+st.title("Attendance (Demo)")
 uploaded_file = st.sidebar.file_uploader("Upload an Excel file", type=["xlsx", "xls"])
 
 # Main content
 if uploaded_file is None:
-    st.title("Attendance (Demo)")
+    
     display_dashboard()
 else:
     if uploaded_file.type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' or uploaded_file.type == 'application/vnd.ms-excel':
